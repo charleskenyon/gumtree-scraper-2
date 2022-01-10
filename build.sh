d0de4bd7a612f580dfbdab110137fbf30188cd9d
Rory Kenyon <rory.kenyon01@gmail.com>
2022-01-10 13:54:37 +0000

    add cdk infrastructure for first lambda

--- build.sh
+++ build.sh
@@ -3,11 +3,38 @@
 STACK_NAME="GumtreeScraperStack"
 REGION=$(aws configure get region)
 ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+LAMBDA_S3_BUCKET=crk-lambda-functions
+
+if [ -z "${REPO_NAME}" ]; then
+    REPO_NAME="$( pwd | sed -e 's/^.*\///' )"
+fi
 
 post_and_exit() {
     echo "${1}"
     exit 1
 }
+
+echo "uploading lambda zips to s3..."
+
+npm run build:lambda
+
+for LAMBDA_DIR in db-iterator-lambda; do
+    echo $LAMBDA_DIR
+    S3_KEY="$REPO_NAME/$LAMBDA_DIR.zip"
+    cp package.json "dist/$LAMBDA_DIR"
+    cp package-lock.json "dist/$LAMBDA_DIR"
+    cd "dist/$LAMBDA_DIR"
+    npm install --silent --only=prod
+    rm *.zip
+    zip -r $LAMBDA_DIR.zip *
+    aws s3 cp --region $REGION "$LAMBDA_DIR.zip" "s3://$LAMBDA_S3_BUCKET/$S3_KEY"
+
+    if [ $LAMBDA_DIR = "db-iterator-lambda" ]; then
+        DB_ITERATOR_LAMBDA_S3_KEY=$S3_KEY
+    fi
+
+    cd ../../
+done
 
 echo "generating cloudformation template..."
 
@@ -15,17 +42,22 @@ npx cdk synth \
     --toolkit-stack-name=$STACK_NAME \
     --context stackName=$STACK_NAME \
     --context region=$REGION \
-    --context accountId=$ACCOUNT_ID --quiet || post_and_exit "cdk synth failed"
+    --context accountId=$ACCOUNT_ID \
+    --context lambdaS3Bucket=$LAMBDA_S3_BUCKET \
+    --context dbIteratorLambdaS3Key=$DB_ITERATOR_LAMBDA_S3_KEY \
+    --quiet || post_and_exit "cdk synth failed"
 
 echo "checking cloudformation template..."
 
 CLOUDFORMATION_TEMPLATE="cdk.out/$STACK_NAME.template.json"
 
-checkov --file $CLOUDFORMATION_TEMPLATE || post_and_exit "checkov validation failed"
+checkov --file $CLOUDFORMATION_TEMPLATE --skip-check "CKV_AWS_116" || post_and_exit "checkov validation failed"
 
 echo "starting cloudformation deployment..."
 
 npx cdk deploy \
     --context stackName=$STACK_NAME \
     --context region=$REGION \
-    --context accountId=$ACCOUNT_ID || post_and_exit "cdk build failed"
+    --context accountId=$ACCOUNT_ID \
+    --context lambdaS3Bucket=$LAMBDA_S3_BUCKET \
+    --context dbIteratorLambdaS3Key=$DB_ITERATOR_LAMBDA_S3_KEY || post_and_exit "cdk build failed"
--- infrastructure/lib/gumtree-scraper-stack.ts
+++ infrastructure/lib/gumtree-scraper-stack.ts
@@ -1,16 +1,41 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps, Tags } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as eventsTargets from 'aws-cdk-lib/aws-events-targets';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GumtreeScraperStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const scraperName = 'gumtree-scraper';
+    const dbIteratorLambdaName = 'db-iterator-lambda';
+    const lambdaS3BucketName = this.node.tryGetContext('lambdaS3Bucket');
+    const dbIteratorLambdaS3Key = this.node.tryGetContext(
+      'dbIteratorLambdaS3Key'
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaS3Bucket = s3.Bucket.fromBucketName(
+      this,
+      'LambdaS3Bucket',
+      lambdaS3BucketName
+    );
+
+    const dbIteratorLambda = new lambda.Function(this, `DbIteratorLambda`, {
+      functionName: `${scraperName}-${dbIteratorLambdaName}`,
+      code: lambda.Code.fromBucket(lambdaS3Bucket, dbIteratorLambdaS3Key),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+    });
+
+    new events.Rule(this, 'DbIteratorLambdaScheduler', {
+      schedule: events.Schedule.rate(Duration.minutes(5)),
+      ruleName: `${scraperName}-${dbIteratorLambdaName}-scheduler`,
+      enabled: false,
+      targets: [new eventsTargets.LambdaFunction(dbIteratorLambda)],
+    });
+
+    Tags.of(this).add('Application', 'Gumtree Scraper');
   }
 }
