be22426f7907d4ae11cf75ce4f2ef2a70882a8b7
Rory Kenyon <rory.kenyon01@gmail.com>
2022-01-14 10:41:37 +0000

    move db iterator lambda infra to construct folder

---
+++ infrastructure/lib/constructs/db-iterator-lambda-construct.ts
@@ -0,0 +1,62 @@
+import { Duration, StackProps } from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as eventsTargets from 'aws-cdk-lib/aws-events-targets';
+import { Construct } from 'constructs';
+
+interface DbIteratorLambdaConstructStackProps extends StackProps {
+  readonly scraperName: string;
+  readonly queryTable: dynamodb.ITable;
+  readonly lambdaS3Bucket: s3.IBucket;
+}
+
+export class DbIteratorLambdaConstruct extends Construct {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: DbIteratorLambdaConstructStackProps
+  ) {
+    super(scope, id);
+
+    const dbIteratorLambdaName = 'db-iterator-lambda';
+    const dbIteratorLambdaS3Key = this.node.tryGetContext(
+      'dbIteratorLambdaS3Key'
+    );
+
+    const dbIteratorLambdaRole = new iam.Role(this, 'DbIteratorLambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'service-role/AWSLambdaBasicExecutionRole'
+        ),
+      ],
+      roleName: `${props.scraperName}-${dbIteratorLambdaName}-role`,
+    });
+
+    dbIteratorLambdaRole.addToPolicy(
+      new iam.PolicyStatement({
+        resources: [props.queryTable.tableArn],
+        actions: ['dynamodb:Scan'],
+      })
+    );
+
+    const dbIteratorLambda = new lambda.Function(this, `DbIteratorLambda`, {
+      functionName: `${props.scraperName}-${dbIteratorLambdaName}`,
+      code: lambda.Code.fromBucket(props.lambdaS3Bucket, dbIteratorLambdaS3Key),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+      reservedConcurrentExecutions: 1,
+      role: dbIteratorLambdaRole,
+    });
+
+    new events.Rule(this, 'DbIteratorLambdaScheduler', {
+      schedule: events.Schedule.rate(Duration.minutes(5)),
+      ruleName: `${props.scraperName}-${dbIteratorLambdaName}-scheduler`,
+      enabled: false,
+      targets: [new eventsTargets.LambdaFunction(dbIteratorLambda)],
+    });
+  }
+}
--- infrastructure/lib/gumtree-scraper-stack.ts
+++ infrastructure/lib/gumtree-scraper-stack.ts
@@ -1,22 +1,15 @@
-import { Duration, Stack, StackProps, Tags } from 'aws-cdk-lib';
+import { Stack, StackProps, Tags } from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as events from 'aws-cdk-lib/aws-events';
-import * as eventsTargets from 'aws-cdk-lib/aws-events-targets';
 import { Construct } from 'constructs';
+import { DbIteratorLambdaConstruct } from './constructs/db-iterator-lambda-construct';
 
 export class GumtreeScraperStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const scraperName = 'gumtree-scraper';
-    const dbIteratorLambdaName = 'db-iterator-lambda';
     const lambdaS3BucketName = this.node.tryGetContext('lambdaS3Bucket');
-    const dbIteratorLambdaS3Key = this.node.tryGetContext(
-      'dbIteratorLambdaS3Key'
-    );
 
     const queryTable = new dynamodb.Table(this, 'QueryTable', {
       partitionKey: { name: 'query', type: dynamodb.AttributeType.STRING },
@@ -32,37 +25,10 @@ export class GumtreeScraperStack extends Stack {
       lambdaS3BucketName
     );
 
-    const dbIteratorLambdaRole = new iam.Role(this, 'DbIteratorLambdaRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName(
-          'service-role/AWSLambdaBasicExecutionRole'
-        ),
-      ],
-      roleName: `${scraperName}-${dbIteratorLambdaName}-role`,
-    });
-
-    dbIteratorLambdaRole.addToPolicy(
-      new iam.PolicyStatement({
-        resources: [queryTable.tableArn],
-        actions: ['dynamodb:Scan'],
-      })
-    );
-
-    const dbIteratorLambda = new lambda.Function(this, `DbIteratorLambda`, {
-      functionName: `${scraperName}-${dbIteratorLambdaName}`,
-      code: lambda.Code.fromBucket(lambdaS3Bucket, dbIteratorLambdaS3Key),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_14_X,
-      reservedConcurrentExecutions: 1,
-      role: dbIteratorLambdaRole,
-    });
-
-    new events.Rule(this, 'DbIteratorLambdaScheduler', {
-      schedule: events.Schedule.rate(Duration.minutes(5)),
-      ruleName: `${scraperName}-${dbIteratorLambdaName}-scheduler`,
-      enabled: false,
-      targets: [new eventsTargets.LambdaFunction(dbIteratorLambda)],
+    new DbIteratorLambdaConstruct(this, 'DbIteratorLambdaConstruct', {
+      scraperName,
+      queryTable,
+      lambdaS3Bucket,
     });
 
     Tags.of(this).add('Application', 'Gumtree Scraper');
